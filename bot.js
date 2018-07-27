cb798a5f08116ecdcd36be94d82b31b89ac2fce9
joshuaj1397 <joshuaj1397@gmail.com>
2018-07-26 21:52:19 -0500

    Added functionality for joining and leaving voice

--- bot.js
+++ bot.js
@@ -1,22 +1,37 @@
-var Discord = require('discord.js');
-var Gracenote = require('node-gracenote');
-var auth = require('./auth.json');
+const Discord = require('discord.js');
+const Gracenote = require('node-gracenote');
+const Gfycat = require('gfycat-sdk');
+const auth = require('./auth.json');
+const Kurisu = require('./KurisuGifs.json');
 
-var graceApi = new Gracenote(auth.gracenote.clientId, auth.gracenote.clientTag, auth.gracenote.userId);
-graceApi.register(function(err, res) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Connected to Gracenote')
-        console.log('User Id: ' + res + '\n');
-    }
+var authGracenote = (graceApi) => {
+    graceApi.register(function(err, res) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('Connected to Gracenote');
+            console.log('User Id: ' + res + '\n');
+            gracenoteUserId = res;
+        }
+    });
+};
+
+var authGfycat = (gfycat) => {
+    gfycat.authenticate((err, data) => {
+    console.log('Connected to Gfycat: ', gfycat, '\n');
 });
+};
 
 var bot = new Discord.Client({
     token: auth.discord.token,
     autorun: true
 });
-bot.login(auth.discord.token);
+
+var gracenoteUserId = null;
+var graceApi = new Gracenote(auth.gracenote.clientId, auth.gracenote.clientTag, gracenoteUserId);
+authGracenote(graceApi);
+
+var gfycat = new Gfycat({clientId: auth.gfycat.clientId, clientSecret: auth.gfycat.clientSecret});
 
 // Listen for messages that start with '*'
 var prefix = '*';
@@ -24,7 +39,19 @@ var prefix = '*';
 bot.on('ready', function(evt) {
     console.log('Connected to Discord');
     console.log('Logged in as: ');
-    console.log(bot.user.username + ' - (' + bot.user.id + ')');
+    console.log(bot.user.username + ' - (' + bot.user.id + ')\n');
+});
+
+// Triggers when a new member is added to the server
+bot.on('guildMemberAdd', member => {
+
+    // TODO: Make the channel and welcome message configurable
+    const channel = member.guild.channels.find('name', 'announcements');
+    if (!channel) {
+        console.log('Channel not found.');
+        return;
+    }
+    channel.send(`Hi ${member}!`);
 });
 
 bot.on('message', message => {
@@ -50,7 +77,53 @@ bot.on('message', message => {
                 message.channel.send(message.content.substring(5));
                 message.delete();
                 break;
+
+            // Leave a voice channel
+            case 'leave':
+            case 'l':
+                leaveVoice(message);
+                break;
+
+            // Join a voice channel
+            case 'join':
+            case 'j':
+                joinVoice(message);
+                break;
+
+            // Search a gif then have the bot post it
+            case 'gfycat':
+                var query = message.content.substring(7);
+                break;
+
             default:
         }
     }
 });
+
+var searchGfy = (gfycat, query) => {
+};
+
+var leaveVoice = (message) => {
+    if (bot.voiceConnections) {
+        bot.voiceConnections.first().disconnect();
+    } else {
+        message.reply('Baka! I\'m not in a voice channel! ' + Kurisu.angry);
+    }
+};
+
+var joinVoice = (message) => {
+    if (!message.guild) return;
+
+    if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+            .then(connection => {
+                var randNum = Math.floor(Math.random() * Kurisu.greeting.length)
+                message.reply(Kurisu.greeting[randNum]);
+            })
+            .catch(console.log);
+    } else {
+        message.reply('You need to join a channel first! ' + Kurisu.angry);
+    }
+};
+
+bot.login(auth.discord.token);
