a82dddcf24938b1baf0b9cda86c1cf6ccbd0723f
joshuaj1397 <joshuaj1397@gmail.com>
2018-07-31 14:50:51 -0500

    Added youtube search functionality

--- bot.js
+++ bot.js
@@ -27,34 +27,39 @@ var authGfycat = (gfycat) => {
 });
 };
 
-// Load client secrets from a local file.
-fs.readFile('googleClientSecret.json', function processClientSecrets(err, content) {
-  if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
-  }
-  // Authorize a client with the loaded credentials
-  authGoogle(JSON.parse(content));
-});
+var processGoogleSecrets = (callback, options) => {
+    fs.readFile('googleClientSecret.json', function processClientSecrets(err, content) {
+        if (err) {
+            console.log('Error loading client secret file: ' + err);
+            return;
+        }
 
-var authGoogle = (credentials, callback) => {
-    var clientSecret = credentials.installed.client_secret;
-    var clientId = credentials.installed.client_id;
-    var redirectUrl = credentials.installed.redirect_uris[0];
-    var oauth2client = new OAuth2(clientId, clientSecret, redirectUrl);
+        // Authorize a client with the loaded credentials, then call the YouTube API.
+        authGoogle(JSON.parse(content), callback, options);
+    });
+}
+
+var authGoogle = (credentials, callback, options) => {
+    var clientSecret = credentials.web.client_secret;
+    var clientId = credentials.web.client_id;
+    var redirectUri = credentials.web.redirect_uris[0];
+    var oauth2client = new OAuth2(clientId, clientSecret, redirectUri);
 
     // Check if we already have a token
     fs.readFile('googleAuth.json', function (err, token) {
+
         if (err) {
             getNewGoogleToken(oauth2client, callback);
         } else {
             oauth2client.credentials = JSON.parse(token);
-            callback(oauth2client);
+            if (callback !== undefined) {
+                callback(oauth2client, options);
+            }
         }
     });
 }
 
-var getNewGoogleToken = (oauth2Client, callback) => {
+var getNewGoogleToken = (oauth2Client, callback, options) => {
     var SCOPES = ['https://www.googleapis.com/auth/youtube.readonly'];
     var authUrl = oauth2Client.generateAuthUrl({
         access_type: 'offline',
@@ -65,21 +70,19 @@ var getNewGoogleToken = (oauth2Client, callback) => {
         input: process.stdin,
         output: process.stdout
     });
-    rl.question('Enter the code from that page here: ', function(code) {
+    rl.question('Enter the code from that page here: ', async function(code) {
         rl.close();
-        oauth2Client.getToken(code, function(err, token) {
-            if (err) {
-                console.log('Error while trying to retrieve access token', err);
-                return;
-            }
-            oauth2Client.credentials = token;
-            storeToken(token);
-            callback(oauth2Client);
-        });
+        const {tokens} = await oauth2Client.getToken(code);
+        oauth2Client.setCredentials(tokens);
+        storeToken(tokens);
+        if (callback !== undefined) {
+            callback(oauth2Client, options);
+        }
     });
 }
 
 var storeToken = (token) => {
+    console.log(token);
     fs.writeFile('googleAuth.json', JSON.stringify(token), (err) => {
         if (err) throw err;
   });
@@ -101,6 +104,7 @@ var gfycat = new Gfycat({clientId: auth.gfycat.clientId, clientSecret: auth.gfyc
 var prefix = '*';
 
 bot.on('ready', function(evt) {
+    processGoogleSecrets();
     console.log('Connected to Discord');
     console.log('Logged in as: ');
     console.log(bot.user.username + ' - (' + bot.user.id + ')\n');
@@ -152,12 +156,15 @@ bot.on('message', message => {
                 message.delete();
                 break;
 
-            // Search a song on youtube in the form of {Artist} {Track}
-            case 'search-youtube':
-            case 's-yt':
+            // Search a song on youtube in the form of {Artist} - {Track}
+            case 'search':
+
+                 // Produces {Artist}-{Track} then splits the artist and track using the '-'
+                args = args.join('');
+                args = args.split('-');
                 var artist = args[0];
                 var track = args[1];
-                searchYoutube(track, artist);
+                processGoogleSecrets(searchYoutube, {artist, track});
                 break;
 
             // Play a song immediately regardless of queue position
@@ -188,10 +195,10 @@ var leaveVoice = (message) => {
         bot.voiceConnections.first().disconnect();
     } else {
 
-        // Let the gif play for 5 secs then delete it
+        // Let the gif play for 15 secs then delete it
         // TODO: Make the time to play the gif configurable
         message.reply('Baka! I\'m not in a voice channel! ' + Kurisu.angry)
-        .then(sent => sent.delete(5 * 1000))
+        .then(sent => sent.delete(15 * 1000))
         .catch(console.log());
     }
 };
@@ -200,47 +207,49 @@ var joinVoice = (message) => {
     if (!message.guild) return;
     if (bot.voiceConnections.first()) {
         message.reply('I\'m already in a voice channel!' + Kurisu.angry)
-        .then(sent => sent.delete(5 * 1000))
+        .then(sent => sent.delete(15 * 1000))
         .catch(console.log());
         return;
-    }
-
-    if (message.member.voiceChannel) {
+    } else if (message.member.voiceChannel) {
         message.member.voiceChannel.join()
             .then(connection => {
                 var randNum = Math.floor(Math.random() * Kurisu.greeting.length)
                 message.reply(Kurisu.greeting[randNum])
-                .then(sent => sent.delete(5 * 1000))
+                .then(sent => sent.delete(15 * 1000))
                 .catch(console.log());
             })
             .catch(console.log);
     } else {
         message.reply('You need to join a channel first! ' + Kurisu.angry)
-        .then(sent => sent.delete(5 * 1000))
+        .then(sent => sent.delete(15 * 1000))
         .catch(console.log());
     }
 };
 
-var searchYoutube = (auth, artist, track) => {
-    var service = google.youtube('v3');
-    var query = artist + track;
-    var parameters = {
-        'params': {
-            'auth': auth,
-            'maxResults': '10',
-            'part': 'snippet',
-            'q': query,
-            'type': 'video', // Videos only
-            'topicId': '/m/04rlf' // Music only
-            'videoDimension': '2d' // 2D videos only
-        }
-    }
+var searchYoutube = async (gAuth, options) => {
+    var service = google.youtube({
+        version: 'v3',
+        gAuth
+    });
+    var artist = options.artist;
+    var track = options.track;
+    var query = artist + ' ' + track;
 
-    service.search.list(parameters, function(err, res) {
+    await service.search.list({
+        auth: gAuth,
+        part: 'snippet',
+        maxResults: '10',
+        q: query,
+        type: 'video', // Videos only
+        topicId: '/m/04rlf', // Music only
+        videoDimension: '2d' // 2D videos only
+    }, function(err, res) {
         if (err) {
             console.log('The API returned an error: ' + err);
-            return;
+        } else {
+
+            // TODO: Output this in a neat message
+            console.log(res.data);
         }
-        console.log(res);
     });
 }
